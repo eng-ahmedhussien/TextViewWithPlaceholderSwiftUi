a5386db790b4cc61436af5a872e3645c6e5b6ba4
ahmed hussien <33827384+eng-ahmedhussien@users.noreply.github.com>
2023-12-04 10:45:17 +0200

    Update TextViewWithPlaceholder.swift
    change   TextViewWithPlaceholder

--- TextViewWithPlaceholder.swift
+++ TextViewWithPlaceholder.swift
@@ -4,72 +4,67 @@
 //
 //  Created by Eslam ALi on 16/08/2023.
 //
-
 import SwiftUI
-import UIKit
 
 
-struct TextViewWithPlaceholder: UIViewRepresentable {
+struct TextViewWithPlaceholder: View {
     @Binding var text: String
     var placeholder: String
+
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            UITextViewWrapper(text: $text)
+                .background(Color.theme.bgInput)
+                .cornerRadius(25)
+                .padding(.leading, -8)
+
+            if $text.wrappedValue.isEmpty {
+                Text(placeholder)
+                    .foregroundColor(Color.theme.txtPlaceholder)
+                    .padding(.leading, 18)
+                    .padding(.top, 14)
+            }
+        }
+    }
+}
+
+struct UITextViewWrapper: UIViewRepresentable {
+    @Binding var text: String
 
     func makeUIView(context: Context) -> UITextView {
         let textView = UITextView()
         textView.delegate = context.coordinator
-        textView.font =  UIFont.preferredFont(forTextStyle: .body)
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
         textView.isScrollEnabled = true
         textView.isEditable = true
         textView.isUserInteractionEnabled = true
-        textView.text = text.isEmpty ? placeholder : text
-        textView.textColor = text.isEmpty ? UIColor(Color.theme.txtPlaceholder) : UIColor(Color.theme.primary)
-        textView.backgroundColor =  UIColor(Color.theme.bgInput)
+        textView.text = text
+        textView.textColor = UIColor(Color.theme.primary)
+        textView.backgroundColor = UIColor(Color.theme.bgInput)
         textView.layer.cornerRadius = 25
-        
-        textView.contentInset = UIEdgeInsets(top: 10, left: -10, bottom: 10, right: -10)
-    
+
+        textView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+
         if LocalizationManager.shared.currentLanguage == .arabic {
             textView.textAlignment = .right
-        }else{
+        } else {
             textView.textAlignment = .left
         }
         return textView
     }
 
     func updateUIView(_ uiView: UITextView, context: Context) {
-        if text.isEmpty {
-            uiView.text = placeholder
-            uiView.textColor = UIColor(Color.theme.txtPlaceholder)
-            if LocalizationManager.shared.currentLanguage == .arabic {
-                uiView.contentInset = UIEdgeInsets(top: 10, left: -10, bottom: 10, right: 10)
-            }else{
-                uiView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: -10)
-            }
-        } else {
-          
-            uiView.text = text
-            uiView.textColor = UIColor(Color.theme.primary)
-            
-            if LocalizationManager.shared.currentLanguage == .arabic {
-                uiView.contentInset = UIEdgeInsets(top: 10, left: -10, bottom: 10, right: 8)
-            }else{
-                uiView.contentInset = UIEdgeInsets(top: 10, left: 8, bottom: 10, right: -10)
-            }
-           
-          
-        }
-        
-
+        uiView.text = text
     }
 
-    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
 
     class Coordinator: NSObject, UITextViewDelegate {
-        var parent: TextViewWithPlaceholder
+        var parent: UITextViewWrapper
 
-        init(_ parent: TextViewWithPlaceholder) {
+        init(_ parent: UITextViewWrapper) {
             self.parent = parent
         }
 
@@ -77,6 +72,10 @@ struct TextViewWithPlaceholder: UIViewRepresentable {
             self.parent.text = textView.text
         }
     }
-    
-    
+}
+
+struct ContentView_PreviewsTextsdkfljasl: PreviewProvider {
+    static var previews: some View {
+        TextViewWithPlaceholder(text: .constant(""), placeholder: "amar")
+    }
 }
